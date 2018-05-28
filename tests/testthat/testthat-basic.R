b498d8372bd8dc48032ad258396411dcd0f77c8f
zzawadz <zygmunt@zstat.pl>
2018-05-28 18:01:38 +0200

    Basic tests

--- tests/testthat/testthat-basic.R
+++ tests/testthat/testthat-basic.R
@@ -1,10 +1,14 @@
 test_that("Basic functions", {
 
   modelPath <- system.file(package = "StarSpaceR", "exdata/model_class")
-
-
   model <- ssr_load_model(modelPath)
-  model$get_vector("wordsfdaa")
-  microbenchmark::microbenchmark(ssr_get_vectors(model, c("words", "topology")))
 
+  vecs <- model$get_vector("words")
+
+  expect_true(!anyNA(vecs))
+  expect_equal(length(vecs), 10)
+
+   model$get_vector("words")
+  expect_equal(ssr_get_vectors(model, "words")[1,],  model$get_vector("words"))
+  expect_equal(dim(ssr_get_vectors(model, c("words", "topology"))), c(2,10))
 })
